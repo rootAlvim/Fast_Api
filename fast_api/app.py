5c7f9ca048e17f513a116de081e910db6e56593f
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-03-03 21:14:34 -0300

    Realizando Primeiro Teste

--- fast_api/app.py
+++ fast_api/app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-#from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse
 from fast_api.schemas import Message
 from http import HTTPStatus
 app = FastAPI()
@@ -7,3 +7,14 @@ app = FastAPI()
 @app.get('/',status_code=HTTPStatus.OK,response_model=Message)
 def read_root():
     return {'message': 'ola mundo'}
+
+@app.get('/home',response_class=HTMLResponse)
+def read_home():
+    return '''
+    <html>
+<body>
+<h1>Hello World</h1>
+</body>
+</html>
+
+'''
---
+++ tests/test_app.py
@@ -0,0 +1,10 @@
+from fastapi.testclient import TestClient
+from fast_api.app import app 
+
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get("/home")
+    assert "text/html" in response.headers["content-type"]
+    assert response.status_code == 200
+    assert "<h1>Hello World</h1>" in response.text
