86434cea6b7e8b7c12af1af2cf52ab12f2c92328
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-03-04 21:07:14 -0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,3 +8,11 @@ def test_read_main():
     assert "text/html" in response.headers["content-type"]
     assert response.status_code == 200
     assert "<h1>Hello World</h1>" in response.text
+
+
+
+
+
+
+
+    #fastapi dev fast_api/app.py --host 0.0.0.0
