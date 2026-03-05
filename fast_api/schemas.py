bb6a2821ea50bc60e31426793a28a63ac1df9722
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-03-04 21:07:19 -0300

    Update schemas.py

--- fast_api/schemas.py
+++ fast_api/schemas.py
@@ -1,3 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+
 class Message(BaseModel):
     message: str
+
+class UserPublic(BaseModel):
+    nome_de_usuario: str 
+    email: EmailStr
+    id: int
+    
+class Userschema(UserPublic):
+    senha: str
+
+class UserDB(Userschema):
+    id: int
