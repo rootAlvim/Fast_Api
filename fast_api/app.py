a3e863489ed20508109227a794479102ddb14721
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-03-04 21:07:09 -0300

    Update app.py

--- fast_api/app.py
+++ fast_api/app.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-from fast_api.schemas import Message
+from fast_api.schemas import Message , Userschema , UserPublic , UserDB
 from http import HTTPStatus
 app = FastAPI()
+
+database = []
 
 @app.get('/',status_code=HTTPStatus.OK,response_model=Message)
 def read_root():
@@ -16,5 +18,16 @@ def read_home():
 <h1>Hello World</h1>
 </body>
 </html>
-
 '''
+
+
+@app.post('/usuario',status_code=201,response_model=UserPublic)
+def criar_usuario(usuario: Userschema):
+    usuario_id = UserDB(
+        nome_de_usuario = usuario.nome_de_usuario,
+        email = usuario.email,
+        senha = usuario.senha,
+        id = len(database)+1
+    )
+    database.append(usuario_id)
+    return usuario_id
