eab6ccedd46981ea9c48e481ebe803dfd81bdf0a
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-03-06 15:05:21 -0300

    Implementando rotas CRUD

--- fast_api/app.py
+++ fast_api/app.py
@@ -1,27 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse 
 from fast_api.schemas import Message , Userschema , UserPublic , UserDB , UserList
-from http import HTTPStatus
+from http import HTTPStatus 
 app = FastAPI()
 
 database = []
-
+'''
 @app.get('/',status_code=HTTPStatus.OK,response_model=Message)
 def read_root():
     return {'message': 'ola mundo'}
 
 @app.get('/home',response_class=HTMLResponse)
 def read_home():
-    return '''
+    return 
     <html>
 <body>
 <h1>Hello World</h1>
 </body>
 </html>
+
 '''
 
-
-@app.post('/usuario',status_code=201,response_model=UserPublic)
+@app.post('/usuario',status_code=201,response_model=UserPublic) #Create
 def criar_usuario(usuario: Userschema):
     usuario_id = UserDB(
         nome_de_usuario = usuario.nome_de_usuario,
@@ -32,6 +32,35 @@ def criar_usuario(usuario: Userschema):
     database.append(usuario_id)
     return usuario_id
 
-@app.get('/users', response_model=UserList)
+@app.get('/users', response_model=UserList) #Read
 def read_users():
     return {'users': database}
+
+
+
+@app.put('/users/{user_id}', status_code=200,response_model=UserPublic) #Update
+def update_user(user_id: int , user:Userschema):
+    usuario_id = UserDB(
+        nome_de_usuario = user.nome_de_usuario,
+        email = user.email,
+        senha = user.senha,
+        id = user_id
+    )
+    if user_id > len(database) or user_id < 1 :
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail = "Usuario não encontrado"
+        )
+    database[user_id - 1] = usuario_id
+    return usuario_id
+
+@app.delete('/users/{user_id}', status_code=200,response_model=UserPublic) #Delete
+#response_model=UserPublic
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1 :
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail = "Usuario não encontrado"
+        )
+    return database.pop(user_id - 1)
+    
--- tests/test_app.py
+++ tests/test_app.py
@@ -6,7 +6,7 @@ from fast_api.app import app
 @pytest.fixture
 def client():
     return TestClient(app)
-    
+'''    
 def test_read_root(client):
    #Arrange(organizacao)
     response = client.get('/') #cliente requisita o / #act(Acao)
@@ -22,7 +22,7 @@ def test_read_home():
     assert "text/html" in response.headers["content-type"]
     assert response.status_code == 200
     assert "<h1>Hello World</h1>" in response.text
-
+''' 
 def test_criar_usuario():
     client = TestClient(app)
     response = client.post(
@@ -55,9 +55,27 @@ def test_read_users(client):
     }
     assert response.json() == json1
 
-
-
-
-
-    
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json= {
+            'nome_de_usuario': 'chico',
+            'email': 'chico@exemplo.com',
+            "senha": "string123",
+        }
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+                'nome_de_usuario': 'chico',
+                'email': 'chico@exemplo.com',
+                'id': 1
+        }
     #fastapi dev fast_api/app.py --host 0.0.0.0
+def test_delete_user(client):
+    response = client.delete("/users/1")
+    assert response.status_code == 200
+    assert response.json() == {
+                'nome_de_usuario': 'chico',
+                'email': 'chico@exemplo.com',
+                'id': 1
+        }
