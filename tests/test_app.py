2440445a3c989583fc4cdaebc457d35e631e54fe
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-03-05 21:24:29 -0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,18 +1,63 @@
+from http import HTTPStatus
+import pytest
 from fastapi.testclient import TestClient
 from fast_api.app import app 
 
-client = TestClient(app)
+@pytest.fixture
+def client():
+    return TestClient(app)
+    
+def test_read_root(client):
+   #Arrange(organizacao)
+    response = client.get('/') #cliente requisita o / #act(Acao)
 
-def test_read_main():
+    assert response.status_code == 200 #assert(afirma)
+    assert response.json() == {'message': 'ola mundo'}
+        
+
+
+def test_read_home():
+    client = TestClient(app)
     response = client.get("/home")
     assert "text/html" in response.headers["content-type"]
     assert response.status_code == 200
     assert "<h1>Hello World</h1>" in response.text
 
+def test_criar_usuario():
+    client = TestClient(app)
+    response = client.post(
+        '/usuario',
+        json={
+            'nome_de_usuario': 'chico',
+            'email': 'chico@exemplo.com',
+            'senha': 'seguro',
+        }
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        
+        'email': 'chico@exemplo.com',
+        'nome_de_usuario': 'chico',
+        'id':1
+    }
+def test_read_users(client):
+    response = client.get('/users')
+    assert response.status_code == 200
+    json1= {
+        'users': [
+            {
+                'nome_de_usuario': 'chico',
+                'email': 'chico@exemplo.com',
+                'id': 1,
+            }
+        ]
+    }
+    assert response.json() == json1
 
 
 
 
 
-
+    
     #fastapi dev fast_api/app.py --host 0.0.0.0
